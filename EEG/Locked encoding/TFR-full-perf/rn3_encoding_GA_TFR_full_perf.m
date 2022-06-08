227ba08334242a6700d0bd923bf45b38174b81b5
Rose Nasrawi <r.nasrawi11@gmail.com>
2022-06-08 17:11:16 +0200

    grand average full TFR performance

---
+++ EEG/Locked encoding/TFR-full-perf/rn3_encoding_GA_TFR_full_perf.m
@@ -0,0 +1,44 @@
+%% Clean workspace
+
+clc; clear; close all
+
+%% Define parameters
+
+subjectIDs = [1:5,7:19,21:27];
+subjects = 1:25;
+
+%% Load data files
+
+for this_subject = subjects
+    
+    %% Parameters
+    sub = subjectIDs(this_subject);
+    [param, eegfiles] = rn3_gen_param(sub);
+    
+    %% load 
+    load([param.path, 'Processed/EEG/Locked encoding/tfr contrasts encoding/' 'full_perf_encoding_' param.subjectIDs{sub}], 'full_perf');
+
+    if this_subject == 1 % Copy structure once for only label, time, freq, dimord
+        full_perf_all = selectfields(full_perf,{'label', 'time', 'freq', 'dimord'});
+    end
+    
+    full_perf_all.load_two_T1_fast_slow(this_subject,:,:,:)         = full_perf.load_two_T1_fast_slow;
+    full_perf_all.load_two_T2_fast_slow(this_subject,:,:,:)         = full_perf.load_two_T2_fast_slow;
+    full_perf_all.load_two_T1_prec_imprec(this_subject,:,:,:)       = full_perf.load_two_T1_prec_imprec;
+    full_perf_all.load_two_T2_prec_imprec(this_subject,:,:,:)       = full_perf.load_two_T2_prec_imprec;
+
+end
+
+%% Average
+
+mean_full_perf_all = selectfields(full_perf_all,{'label', 'time', 'freq', 'dimord'});
+
+mean_full_perf_all.load_two_T1_fast_slow         = squeeze(mean(full_perf_all.load_two_T1_fast_slow));
+mean_full_perf_all.load_two_T2_fast_slow         = squeeze(mean(full_perf_all.load_two_T2_fast_slow));
+mean_full_perf_all.load_two_T1_prec_imprec       = squeeze(mean(full_perf_all.load_two_T1_prec_imprec));
+mean_full_perf_all.load_two_T2_prec_imprec       = squeeze(mean(full_perf_all.load_two_T2_prec_imprec));
+
+%% Save
+
+save ([param.path, '/Processed/EEG/Locked encoding/tfr contrasts encoding/full_perf_all'], 'full_perf_all');
+save ([param.path, '/Processed/EEG/Locked encoding/tfr contrasts encoding/mean_full_perf_all'], 'mean_full_perf_all');
